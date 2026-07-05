a30d92237281e3b0b07d4c532aa12ffe9d6c0e86
Dmitry Konishchev <konishchev@gmail.com>
2026-07-05 15:50:30 +0300

    Implement `other` broker statement parsing

---
+++ src/broker_statement/other/config.rs
@@ -0,0 +1,38 @@
+use serde::Deserialize;
+
+use crate::time::{deserialize_date, deserialize_optional_date};
+use crate::types::{Date, Decimal};
+
+#[derive(Deserialize)]
+#[serde(tag = "type", rename_all = "kebab-case")]
+pub enum Operation {
+    Buy(TradeOperation),
+    Sell(TradeOperation),
+    Dividend(DividendOperation),
+}
+
+#[derive(Deserialize)]
+#[serde(deny_unknown_fields)]
+pub struct TradeOperation {
+    #[serde(deserialize_with = "deserialize_date")]
+    pub date: Date,
+    #[serde(default, deserialize_with = "deserialize_optional_date")]
+    pub settle_date: Option<Date>,
+
+    pub symbol: String,
+    pub quantity: Decimal,
+
+    pub price: Decimal,
+    pub amount: Decimal,
+}
+
+#[derive(Deserialize)]
+#[serde(deny_unknown_fields)]
+pub struct DividendOperation {
+    #[serde(deserialize_with = "deserialize_date")]
+    pub date: Date,
+    pub symbol: String,
+    pub gross_amount: Decimal,
+    pub tax_withheld: Decimal,
+    pub net_amount: Decimal,
+}
---
+++ src/broker_statement/other/mod.rs
@@ -0,0 +1,167 @@
+pub mod config;
+
+use isin::ISIN;
+
+#[cfg(test)] use crate::brokers::Broker;
+#[cfg(test)] use crate::config::Config;
+use crate::core::{EmptyResult, GenericResult};
+use crate::currency::{Cash, CashAssets};
+use crate::exchanges::Exchange;
+use crate::formatting;
+use crate::instruments::{self, InstrumentId};
+use crate::time::{self, Period};
+use crate::util::{self, DecimalRestrictions};
+
+#[cfg(test)] use super::{BrokerStatement, ReadingStrictness};
+use super::partial::PartialBrokerStatement;
+use super::trades::{StockBuy, StockSell};
+
+use self::config::{Operation, TradeOperation, DividendOperation};
+
+pub struct StatementParser<'a> {
+    currency: &'a str,
+    statement: PartialBrokerStatement,
+}
+
+impl<'a> StatementParser<'a> {
+    pub fn parse(operations: &[Operation], currency: &str) -> GenericResult<PartialBrokerStatement> {
+        let mut parser = StatementParser {
+            currency: currency,
+            statement: PartialBrokerStatement::new(&[Exchange::Otc], true),
+        };
+
+        let today = time::today();
+        let mut open_date = today;
+
+        for operation in operations {
+            let date = match operation {
+                Operation::Buy(trade) => {
+                    parser.parse_trade(trade, true).map_err(|e| format!(
+                        "Invalid buy operation at {}: {e}", formatting::format_date(trade.date)))?;
+                    trade.date
+                },
+                Operation::Sell(trade) => {
+                    parser.parse_trade(trade, false).map_err(|e| format!(
+                        "Invalid sell operation at {}: {e}", formatting::format_date(trade.date)))?;
+                    trade.date
+                },
+                Operation::Dividend(dividend) => {
+                    parser.parse_dividend(dividend).map_err(|e| format!(
+                        "Invalid dividend operation at {}: {e}", formatting::format_date(dividend.date)))?;
+                    dividend.date
+                },
+            };
+            open_date = std::cmp::min(open_date, date);
+        }
+
+        let period = Period::new(open_date, today)?;
+        parser.statement.period = Some(period);
+        parser.statement.set_has_starting_assets(false)?;
+
+        Ok(parser.statement)
+    }
+
+    fn parse_trade(&mut self, trade: &TradeOperation, buy: bool) -> EmptyResult {
+        self.on_symbol(&trade.symbol)?;
+
+        let conclusion_time = trade.date.into();
+        let execution_date = trade.settle_date.unwrap_or(trade.date);
+
+        let quantity = util::validate_named_decimal("quantity", trade.quantity, DecimalRestrictions::StrictlyPositive)?;
+        let price = util::validate_named_cash("price", self.currency, trade.price, DecimalRestrictions::StrictlyPositive)?;
+        let amount = util::validate_named_cash("amount", self.currency, trade.amount, DecimalRestrictions::StrictlyPositive)?;
+        let commission = Cash::zero(self.currency);
+
+        let expected_amount = price * quantity;
+        if amount.round() != expected_amount.round() {
+            return Err!("Got an unexpected amount: {amount} vs {expected_amount}");
+        }
+
+        if buy {
+            self.statement.deposits_and_withdrawals.push(
+                CashAssets::new_from_cash(trade.date, amount));
+
+            self.statement.stock_buys.push(StockBuy::new_trade(
+                &trade.symbol, quantity, price, amount, commission,
+                conclusion_time, execution_date));
+        } else {
+            self.statement.stock_sells.push(StockSell::new_trade(
+                &trade.symbol, quantity, price, amount, commission,
+                conclusion_time, execution_date, false));
+
+            self.statement.deposits_and_withdrawals.push(
+                CashAssets::new_from_cash(trade.date, -amount));
+        }
+
+        Ok(())
+    }
+
+    fn parse_dividend(&mut self, dividend: &DividendOperation) -> EmptyResult {
+        let isin = self.on_symbol(&dividend.symbol)?;
+
+        let gross_amount = util::validate_named_cash(
+            "gross amount", self.currency, dividend.gross_amount, DecimalRestrictions::StrictlyPositive)?;
+
+        let tax_withheld = util::validate_named_cash(
+            "tax withheld amount", self.currency, dividend.tax_withheld, DecimalRestrictions::PositiveOrZero)?;
+
+        let net_amount = util::validate_named_cash(
+            "net amount", self.currency, dividend.net_amount, DecimalRestrictions::StrictlyPositive)?;
+
+        if gross_amount - tax_withheld != net_amount {
+            return Err!("{gross_amount} - {tax_withheld} != {net_amount}");
+        }
+
+        let issuer_id = InstrumentId::Isin(isin);
+        self.statement.dividend_accruals(dividend.date, issuer_id.clone(), true).add(dividend.date, gross_amount);
+        self.statement.tax_accruals(dividend.date, issuer_id, true).add(dividend.date, tax_withheld);
+
+        self.statement.deposits_and_withdrawals.push(
+            CashAssets::new_from_cash(dividend.date, -net_amount));
+
+        Ok(())
+    }
+
+    fn on_symbol(&mut self, symbol: &str) -> GenericResult<ISIN> {
+        // At this time `other` broker type is implied to be used for closed-end unit investment funds only which don't
+        // have tickers, but have ISIN. On the other side, now we need ISIN information for dividend processing. So at
+        // least for now make this requirement to simplify the things.
+        let isin = instruments::parse_isin(symbol)?;
+        self.statement.instrument_info.get_or_add(symbol).add_isin(isin);
+        Ok(isin)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_real() {
+        // FIXME(konishchev): Prepare testdata
+        // let config = Config::new("testdata/configs/main", None).unwrap();
+        let config = Config::new(".", None).unwrap();
+
+        let portfolio = config.get_portfolio("close-end").unwrap();
+        assert_eq!(portfolio.broker, Broker::Other);
+
+        let statement = BrokerStatement::load(&config, portfolio, ReadingStrictness::all()).unwrap();
+
+        assert!(statement.assets.cash.is_empty());
+        assert!(statement.assets.other.is_none());
+        assert!(!statement.deposits_and_withdrawals.is_empty());
+
+        assert!(statement.fees.is_empty());
+        assert!(statement.cash_grants.is_empty());
+        assert!(statement.idle_cash_interest.is_empty());
+        assert!(statement.tax_agent_withholdings.is_empty()); // FIXME(konishchev): Support + test
+
+        assert!(statement.forex_trades.is_empty());
+        assert!(!statement.stock_buys.is_empty());
+        assert!(statement.stock_sells.is_empty()); // FIXME(konishchev): Test
+        assert!(!statement.dividends.is_empty());
+
+        assert!(!statement.open_positions.is_empty());
+        assert!(!statement.instrument_info.is_empty());
+    }
+}
